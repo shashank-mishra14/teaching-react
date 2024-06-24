18e25588ae633fd34ab9e5ef5dd0d9c324647bdb
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-06-24 18:53:47 +0530

    changes in the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import styles from "./style";
 
-import { Navbar } from "./components";
+import { Navbar, Hero } from "./components";
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
+        </div>
+      </div>
+      <div className={` bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
         </div>
       </div>
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 import Navbar from "./Navbar"
-
-export { Navbar };
+import Hero from "./Hero"
+export { Navbar,Hero };
