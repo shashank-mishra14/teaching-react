880430668ec039bdcb747d8fd2e680be9d44639b
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-06-24 18:29:07 +0530

    creating a navbar with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./style";
+
+import { Navbar } from "./components";
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,17 @@
+
+export const navLinks=[
+    {
+        id:"home",
+        title: "Home"
+    },
+    {
+        id:"features",
+        title: "Features"
+    },{
+        id:"product",
+        title: "Product"
+    },{
+        id:"Clients",
+        title: "Clients"
+    }
+]
