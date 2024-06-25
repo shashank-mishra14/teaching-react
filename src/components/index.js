ace41b522ec02bf50cbc395fe9d127d3788a9fad
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-06-25 18:35:46 +0530

    created the business card and button components

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ import Hero from "./Hero";
 import GetStarted from "./GetStarted";
 import Stats from "./Stats";
 import Business from "./Business";
-export { Navbar, Hero, GetStarted, Stats, Business };
+import Button from "./Button";
+export { Navbar, Hero, GetStarted, Stats, Business, Button };
